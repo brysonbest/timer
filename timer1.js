8fb86ef4134163918bc32fb074eb0d9ee0deb178
Bryson Best <bryson.best@gmail.com>
2021-11-25 17:15:19 -0800

    Refactor and modularize.

--- timer1.js
+++ timer1.js
@@ -1,13 +1,21 @@
 //takes in numbers from the console and creates an alert sound in seconds based on this input
 
+const scheduleBeep = function(time) {
+  let newTime = Number(time);
+  if (isNaN(newTime) || newTime <= 0) {
+    return;
+  }
+
+  setTimeout(() => {
+    process.stdout.write('\x07');
+    console.log("beep", newTime);
+  }, newTime * 100);
+};
+
 const times = process.argv.slice(2);
 
-for (let time of times) {
-  let newTime = Number(time);
-  if (typeof newTime === "number" && newTime > 0) {
-    setTimeout(() => {
-      process.stdout.write('\x07');
-    }, newTime * 100);
-  }
+for (const time of times) {
+  scheduleBeep(time);
 }
+
 console.log();
